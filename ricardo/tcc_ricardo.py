5c4f5b25db3a597e9c19a8014662d73f562f1a0f
Neylson Crepalde <neylsoncrepalde@gmail.com>
2016-09-19 10:41:39 -0300

    Update tcc_ricardo.py

--- ricardo/tcc_ricardo.py
+++ ricardo/tcc_ricardo.py
@@ -7,8 +7,6 @@ import pandas as pd
 import numpy as np
 import scipy as sp
 import matplotlib.pyplot as plt
-from matplotlib.patches import Polygon
-
 
 dados = pd.read_csv('~/Documentos/tcc_ricardo.csv')
 
@@ -21,5 +19,20 @@ plt.xlabel('Idade dos entrevistados')
 plt.show()
 
 dados.columns.values
+pd.crosstab()
+tabela = pd.crosstab(index = dados['20.1'],
+                     columns = 'count')
+tabela.columns.values
+tabela = tabela['count']
+counts = tabela.values
 
+
+#plt.bar(, height=counts, width=1, color='yellow')
+plt.show()
+
+plt.bar(dados['20.1'], bins=5)
+'''
 plt.hist(dados['20.2'], bins=5)
+plt.hist(dados['20.3'], bins=5)
+plt.hist(dados['20.4'], bins=5)
+'''

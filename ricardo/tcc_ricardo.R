8f3b9bb84bea61277f9f70a18bcdd9a781e89f7a
Neylson Crepalde <neylsoncrepalde@gmail.com>
2016-09-19 09:11:55 -0300

    Update tcc_ricardo.R

--- ricardo/tcc_ricardo.R
+++ ricardo/tcc_ricardo.R
@@ -10,13 +10,14 @@ names(dados) <- c('quest','class','sexo','escolaridade','motivacao','setor',
                   'mercado','tempo_atuacao','idade','contratos_efet',
                   'segmento_contratos','tempo_agenciado','investimento',
                   'retorno','empenho_escola','gerenc_carreira','confianca',
-                  'melhoras','imagem','20_preparado','20_conhecimetnos',
+                  'melhoras','imagem','20_preparado','20_conhecimentos',
                   '20_rede','20_expressao','contrib_escola','preparacao',
                   'oportunidades','comentario_final')
 
 library(descr)
 library(magrittr)
 library(xtable)
+library(ggplot2)
 
 dim(dados)
 
@@ -113,6 +114,93 @@ levels(dados$empenho_escola) <- c('Muito abaixo do esperado','Abaixo do esperado
 
 freq(dados$empenho_escola, plot=F)
 
-# 16 
+# 16 gerenciamento de carreira: o que é mais importante
+dados$gerenc_carreira %<>% as.factor
+levels(dados$gerenc_carreira) <- c('Formação Técnica','Divulgação em mercado audiovisual',
+                                   'Preparação pessoal para o mercado',
+                                   'Preparação para testes de elenco', 'Outro')
+freq(dados$gerenc_carreira, plot=F)
 
+# 17 Sente segurança e confiança na sua formação?
+dados$confianca %<>% as.factor
+levels(dados$confianca) <- c('Muita insegurança','Sinto insegurança','Seguro',
+                             'Seguro e confiante','Muito seguro e confiante')
+freq(dados$confianca, plot=F)
 
+# 18 percebeu melhoras no desempenho por conta da escola?
+dados$melhoras %<>% as.factor
+levels(dados$melhoras) <- c('Muito abaixo do esperado','Abaixo do esperado',
+                            'Dentro do Esperado','Significativo',
+                            'Muito significativo', 'Não sei responder')
+freq(dados$melhoras, plot=F)
+
+#=========================================
+# BLOCO III
+#=========================================
+
+# 19 imagem profissional está bem aplicada no mercado?
+dados$imagem %<>% as.factor
+levels(dados$imagem) <- c('Muito abaixo do esperado','Abaixo do esperado',
+                          'Dentro do Esperado','Significativo',
+                          'Muito significativo', 'Não sei responder')
+freq(dados$imagem, plot=F)
+
+# 20 questões de avaliação de posição no mercado
+dados$`20_preparado` %<>% as.factor
+dados$`20_conhecimentos` %<>% as.factor
+dados$`20_rede` %<>% as.factor
+dados$`20_expressao` %<>% as.factor
+
+levels(dados$`20_preparado`) <- c('Discordo totalmente','Discordo',
+                                  'Nem concordo nem discordo',
+                                  'Concordo', 'Concordo plenamente')
+levels(dados$`20_conhecimentos`) <- c('Discordo totalmente','Discordo',
+                                  'Nem concordo nem discordo',
+                                  'Concordo', 'Concordo plenamente')
+levels(dados$`20_rede`) <- c('Discordo totalmente','Discordo',
+                                  'Nem concordo nem discordo',
+                                  'Concordo', 'Concordo plenamente')
+levels(dados$`20_expressao`) <- c('Discordo totalmente','Discordo',
+                                  'Nem concordo nem discordo',
+                                  'Concordo', 'Concordo plenamente')
+
+freq(dados$`20_preparado`, plot=F)
+freq(dados$`20_conhecimentos`, plot=F)
+freq(dados$`20_rede`, plot=F)
+freq(dados$`20_expressao`, plot=F)
+
+g1 <- ggplot(dados, aes(`20_preparado`))+geom_bar()+coord_flip()+theme_light()+
+  labs(y="Porcentagens",x="Preparado")
+g2 <- ggplot(dados, aes(`20_conhecimentos`))+geom_bar()+coord_flip()+theme_light()+
+  labs(y="Porcentagens",x="Conhecimentos")
+g3 <- ggplot(dados, aes(`20_rede`))+geom_bar()+coord_flip()+theme_light()+
+  labs(y="Porcentagens",x="Rede")
+g4 <- ggplot(dados, aes(`20_expressao`))+geom_bar()+coord_flip()+theme_light()+
+  labs(y="Porcentagens",x="Expressão")
+
+# carregar funcao multiplot
+multiplot(g1,g2,g3,g4, cols = 2)
+
+# 21 qualidade da contribuicao da escola
+dados$contrib_escola %<>% as.factor
+levels(dados$contrib_escola) <- c('Abaixo do esperado',
+                                  'Dentro do Esperado','Significativo',
+                                  'Muito significativo')
+freq(dados$contrib_escola, plot=F)
+
+# 22 Como voce se prepara para testes de elenco MUITO INTERESSANTE
+dados$preparacao %<>% as.factor
+levels(dados$preparacao) <- c('Decoro o texto antes','Decora o texto e busca informações adicionais',
+                              'Não decora o texto e foca em exercícios espontâneos',
+                              'Estuda e cria comportamentos para o personagem antes do teste')
+freq(dados$preparacao, plot=F)
+
+# 23 oportunidades em audiovisual em BH
+dados$oportunidades %<>% as.factor
+levels(dados$oportunidades) <- c('Muito abaixo do esperado','Abaixo do esperado',
+                                 'Dentro do Esperado','Significativo',
+                                 'Muito significativo', 'Não sei responder')
+freq(dados$oportunidades, plot=F)
+
+#24 comentario final
+freq(dados$comentario_final, plot=F)
